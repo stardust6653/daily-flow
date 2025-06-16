368638effa4e702042a89dd5d351057bac7c9741
soye <stardust6653@gmail.com>
2025-06-16 21:32:22 +0900

    feat : 완료 후 사용금액 총합 계산 후 타이틀 옆 렌더링

--- apps/daily-flow-app/app/_core/components/pages/main/MainSection/index.tsx
+++ apps/daily-flow-app/app/_core/components/pages/main/MainSection/index.tsx
@@ -1,10 +1,11 @@
 import MainTitle from "../MainTitle";
 import { MainSectionListStyle, MainSectionStyle } from "./MainSection.css";
 import TaskList from "../TaskList";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import AddStatusButton from "../AddStatusButton";
 import { CategoryType, TaskStatusType, TaskType } from "@/types/types";
 import CompleteTaskList from "../CompleteTaskList";
+import { ModalType } from "@/types/task";
 
 interface MainSectionProps {
   taskStatus: TaskStatusType[];
@@ -12,7 +13,7 @@ interface MainSectionProps {
   selectedCategory: string;
   tasks: TaskType[];
   refreshData: () => void;
-  setIsModalOpen: Dispatch<SetStateAction<{ isOpen: boolean; type: string }>>;
+  setIsModalOpen: Dispatch<SetStateAction<ModalType>>;
 }
 
 const MainSection = ({
@@ -27,11 +28,24 @@ const MainSection = ({
     (item) => item.id === selectedCategory
   );
 
+  const [totalExpenditure, setTotalExpenditure] = useState(0);
+
+  useEffect(() => {
+    const total = tasks.reduce(
+      (sum, task) =>
+        task.complete ? sum + Number(task.expenditure || 0) : sum,
+      0
+    );
+
+    setTotalExpenditure(total);
+  }, [tasks]);
+
   return (
     <>
       <section className={MainSectionStyle}>
         {selectedCategoryData && (
           <MainTitle
+            expenditure={totalExpenditure}
             data={selectedCategoryData}
             setIsModalOpen={setIsModalOpen}
           />
--- apps/daily-flow-app/app/_core/components/pages/main/MainTitle/index.tsx
+++ apps/daily-flow-app/app/_core/components/pages/main/MainTitle/index.tsx
@@ -11,12 +11,16 @@ import { Dispatch, SetStateAction } from "react";
 interface MainTitleProps {
   data: CategoryType;
   setIsModalOpen: Dispatch<SetStateAction<{ isOpen: boolean; type: string }>>;
+  expenditure: number;
 }
 
-const MainTitle = ({ data, setIsModalOpen }: MainTitleProps) => {
+const MainTitle = ({ data, setIsModalOpen, expenditure }: MainTitleProps) => {
   const handleDeleteClick = () => {
     setIsModalOpen({ isOpen: true, type: "deleteCategory" });
   };
+
+  const expenditureText =
+    expenditure > 0 ? `- ${expenditure.toLocaleString()}원 사용` : "";
 
   return (
     <div className={MainTitleStyle}>
@@ -25,7 +29,9 @@ const MainTitle = ({ data, setIsModalOpen }: MainTitleProps) => {
           className={MainTitleColorChipStyle}
           style={{ backgroundColor: data?.color }}
         />
-        <h2>{data?.name}</h2>
+        <h2>
+          {data?.name} {expenditureText}
+        </h2>
       </div>
       <FaTrashCan className={DeleteIconStyle} onClick={handleDeleteClick} />
     </div>
